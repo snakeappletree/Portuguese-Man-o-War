640a719bba34466c4d8d6e8d3c6ff0a4f261195b
snakeappletree <130707223+snakeappletree@users.noreply.github.com>
2024-06-03 20:59:43 -0700

    Update __init__.py
    trying to ensure recognition of package still ooof

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from count import main as count_main
-from dumpquery import main as dumpquery_main
-from histo import main as histo_main
-from randomDNA import main as randomDNA_main
+from count import main as countm
+from dumpquery import main as dumpquerym
+from histo import main as histom
+from randomDNA import main as randomDNAm
